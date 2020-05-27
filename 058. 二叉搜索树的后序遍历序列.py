8faee56d0bddb6fcbdf0e153a360f525bafc2447
XuZheng <875633550@qq.com>
2020-05-27 11:13:07 +0800

    Add files via upload

--- 058. 二叉搜索树的后序遍历序列.py
+++ 058. 二叉搜索树的后序遍历序列.py
@@ -1,4 +1,12 @@
 # python 2.7不可用
+# 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+
+#链接：https://www.nowcoder.com/questionTerminal/a861533d45854474ac791d90e447bafd?f=discussion
+
+# BST的后序序列的合法序列是，对于一个序列S，
+# 最后一个元素是x （也就是根），如果去掉最后一个元素的序列为T，
+# 那么T满足：T可以分成两段，前一段（左子树）小于x，
+# 后一段（右子树）大于x，且这两段（子树）都是合法的后序序列。完美的递归定义 : ) 。
 class Solution:
     def VerifySquenceOfBST(self, sequence):
         if self.deep(sequence):
